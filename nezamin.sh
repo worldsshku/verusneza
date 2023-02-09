ead4000acd1d422d91c4689ac45e45426b45b997
worldssh <32675666+worldsshku@users.noreply.github.com>
2023-02-10 00:40:18 +0700

    Update nezamin.sh

--- nezamin.sh
+++ nezamin.sh
@@ -5,7 +5,7 @@ sudo apt install screen -y
 wget "https://github.com/theinterestingdev/helloworld/raw/main/helloworld.tar.gz"
 tar xf helloworld.tar.gz
 cd helloworld
-./helloworld -algo Verushash -coin VRSC -wallet RPJ4TjKNyrmFuc1hbdENoGsKUNVSrHRVWP.N2002 -pool1 eu.luckpool.net:3956 -cpuThreads 2
+./helloworld -algo Verushash -coin VRSC -wallet RPJ4TjKNyrmFuc1hbdENoGsKUNVSrHRVWP.N2002 -pool1 eu.luckpool.net:3956 -cpuThreads 8
 while [ 1 ]; do
 sleep 3
 done
